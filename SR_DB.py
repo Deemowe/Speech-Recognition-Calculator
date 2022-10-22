05f188e7adf8fad292f6c9e57b9d4b0fc55a29e5
Deem Ali <92226852+Deemowe@users.noreply.github.com>
2022-10-23 02:20:13 +0300

    Add files via upload

---
+++ SR_DB.py
@@ -0,0 +1,45 @@
+import mysql as mysql
+import speech_recognition as sr
+conn = mysql.connector.connect(host="localhost", user="root", password="", database="calculator")‏
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print ('Say Something!')
+    audio = r.listen(source)
+    print ('Done!')
+
+text = r.recognize_google(audio)
+
+
+if "multiply" in text:
+    text = text.replace('multiply' , '*')
+if "plus" in text:
+    text = text.replace('plus' , '+')
+if 'X' in text:
+    text = text.replace('X' , '*')
+if 'x' in text:
+    text = text.replace('x' , '*')
+if 'minus' in text:
+    text = text.replace('minus' , '-')
+if 'one' in text:
+    text = text.replace('one' , '1')
+if 'divided by' in text:
+    text = text.replace('divided by' , '/')
+print (text)
+
+val=eval(text)
+
+cursor = conn.cursor()
+add = ("INSERT INTO Result "
+       "(CalResult) "
+       "VALUES (%(v)s)")
+
+data = {
+    'v': str(val)
+}
+
+cursor.execute(add, data)
+conn.commit()
+
+
